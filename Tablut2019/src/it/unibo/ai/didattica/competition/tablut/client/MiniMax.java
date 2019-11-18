57df156d11fc1080e56810c6d7600a4cf75806a3
Administrator <Administrator@LAPTOP-8335SPHH>
2019-11-18 21:33:24 +0100

    che schifo

--- Tablut2019/src/it/unibo/ai/didattica/competition/tablut/client/MiniMax.java
+++ Tablut2019/src/it/unibo/ai/didattica/competition/tablut/client/MiniMax.java
@@ -17,9 +17,16 @@ public class MiniMax {
 	public static final int DRAW = -1;
 	public static final int NEUTRAL = 0;
 
+	public  int maxUtility = 0;
 	private Game game;
 	private Turn player;
+	private Integer maxDepth = 3;
 
+	/**
+	 * 
+	 * @param game
+	 * @param player player which the CPU wants to win
+	 */
 	public MiniMax(Game game, Turn player) {
 		super();
 		this.game = game;
@@ -28,53 +35,56 @@ public class MiniMax {
 
 
 	public State minimaxDecision(State state) {
-		int v = maxValue(state);
-		System.out.println("V: " + v);
-		for (State s : successors(state)) {
-			System.out.println("------- utility: " + utility(s) + ",    v : " + v);
+		Integer depth = 0;
+		LinkedList<State> successors = successors(state);
+		int max = Integer.MIN_VALUE;
+		State bestMove = null;
+		int v;
 
-			if (utility(s) == v) 
-			{
-				
-				return s;
+		for (State s : successors) {
+			v = maxValue(s, depth -1);
+			
+			if (v > max) {
+				max = v;
+				bestMove = s;
 			}
 		}
-		return null;
+		System.out.println("max: " + max);
+		return bestMove;
+
 	}
 
-	public int maxValue(State state) {
-		if (terminalTest(state))  {
-			System.out.println("(maxvalue)"+state+"terminal! utility: " + utility(state));
-			return utility(state);
-		}
+	public int maxValue(State state, Integer depth) {
+		depth = depth + 1;
+		if (terminalTest(state) || depth == maxDepth) return utility(state);
 		int v = Integer.MIN_VALUE;
-		for (State s : successors(state)) {
-			v = Math.max(v, minValue(s));
-		}
+
+		for (State s : successors(state)) 			
+			v = Math.max(v, minValue(s, depth));
+
 		return v;
 	}
 
-	public int minValue(State state) {
-		if (terminalTest(state)) {
-			System.out.println("(minvalue) "+state+"terminal! utility: " + utility(state));
-			return utility(state);
-		}
+	public int minValue(State state, Integer depth) {
+		depth = depth + 1;
+		if (terminalTest(state) || depth == maxDepth) return utility(state);
 		int v = Integer.MAX_VALUE;
-		for (State s : successors(state)) {
-			v = Math.min(v, maxValue(s));
-		}
+
+		for (State s : successors(state)) 
+			v = Math.min(v, maxValue(s, depth));
+
 		return v;
 	}
 
 	public boolean terminalTest(State state) {
-		
+
 		if (state.getTurn().equals(Turn.BLACKWIN) ||
 				state.getTurn().equals(Turn.WHITEWIN) || 
 				state.getTurn().equals(Turn.DRAW)) {
 			System.out.println("TURNO TERMINALE: " + state.getTurn());
-		return true;
+			return true;
 		} else return false;
-		
+
 	}
 
 
@@ -120,45 +130,6 @@ public class MiniMax {
 
 
 	/**
-	 * @param row
-	 * @param column
-	 * @param state
-	 * @return List of all the possible next states for a specific pawn of a faction, in all the four directions
-	 */
-	public LinkedList<State> successors(int row, int col, State state) {
-		//System.out.println("Computing the moves from cell " + state.getBox(row, col));
-		LinkedList<State> elegibiles = new LinkedList<>();
-		LinkedList<String> toTry = new LinkedList<>();
-		String from = state.getBox(row, col);
-		Pawn[][] board = state.getBoard();
-		int dim = board.length;
-
-		// up
-		for (int r = row-1; r >= 0; r--)	
-			if (board[r][col].equals(Pawn.EMPTY)) toTry.add(state.getBox(r, col));	else break;// free cells on vertical axis 
-		// right
-		for (int c = col+1; c < dim; c++)	
-			if (board[row][c].equals(Pawn.EMPTY)) toTry.add(state.getBox(row, c));	else break;// free cells on horizontal axis 
-		// down
-		for (int r = row+1; r < dim; r++)	
-			if (board[r][col].equals(Pawn.EMPTY)) toTry.add(state.getBox(r, col));	else break; // free cells on vertical axis 
-		// elft
-		for (int c = col-1; c >= 0; c--)	
-			if (board[row][c].equals(Pawn.EMPTY)) toTry.add(state.getBox(row, c));	else break;// free cells on horizontal axis 
-
-
-		for (String to:toTry) {
-			try {
-				/*TODO checkMove modifica lo stato ad ogni controllo, quindi serve fare una copia dello stato ogni volta
-				 * GameAshtonTablut.java riga 268  */
-				elegibiles.add(game.checkMove(state.clone(), new Action(from, to, state.getTurn())).clone());
-			} catch (Exception e) { /*System.out.println("\tNO"); */}
-		}
-		return elegibiles;
-	}
-
-
-	/**
 	 * 
 	 * @param state
 	 * @param turn
@@ -166,44 +137,73 @@ public class MiniMax {
 	 * @return List of all the possible next states for a player
 	 */
 	public LinkedList<State> successors(State state) {
+
 		LinkedList<State> elegibiles = new LinkedList<>();
-		Pawn[][] board = state.getBoard();
-		int dim = board.length;
-		for (int r = 0; r < dim; r++)
-			for (int c = 0; c < dim; c++)
-				if (board[r][c].toString().equals(state.getTurn().toString())) {
-					//System.out.println("Trovata pedina " + turn);
-					elegibiles.addAll(successors(r, c, state));
-				} //else System.out.println("pedina diversa " + board[r][c]);
+		if (!terminalTest(state)) {
+			Pawn[][] board = state.getBoard();
+			int dim = board.length;
+			for (int row = 0; row < dim; row++)
+				for (int col = 0; col < dim; col++)
+					if (board[row][col].toString().equals(state.getTurn().toString()) || 
+							( state.getTurn().equals(Turn.WHITE) && board[row][col].toString().equals(Pawn.KING.toString()))) {
+						LinkedList<String> toTry = new LinkedList<>();
+						String from = state.getBox(row, col);
+
+						// up
+						for (int r = row-1; r >= 0; r--)	
+							if (board[r][col].equals(Pawn.EMPTY)) toTry.add(state.getBox(r, col));	else break;// free cells on vertical axis 
+						// right
+						for (int c = col+1; c < dim; c++)	
+							if (board[row][c].equals(Pawn.EMPTY)) toTry.add(state.getBox(row, c));	else break;// free cells on horizontal axis 
+						// down
+						for (int r = row+1; r < dim; r++)	
+							if (board[r][col].equals(Pawn.EMPTY)) toTry.add(state.getBox(r, col));	else break; // free cells on vertical axis 
+						// elft
+						for (int c = col-1; c >= 0; c--)	
+							if (board[row][c].equals(Pawn.EMPTY)) toTry.add(state.getBox(row, c));	else break;// free cells on horizontal axis 
+
+						for (String to:toTry) {
+							try {
+								elegibiles.add(game.checkMove(state.clone(), new Action(from, to, state.getTurn())).clone());
+							} catch (Exception e) { /*System.out.println("\tNO"); */}
+						}
+					} 
+		}
 		return elegibiles;
 	}
 
 
 	public static void main(String args[]) {
 		Game game = new GameAshtonTablut(99, 0, "garbage", "fake", "fake");
-		MiniMax m = new MiniMax(game, Turn.BLACK);
-				
-		State s0 = new StateTablut();
-		
+		MiniMax m = new MiniMax(game, Turn.WHITE);
+
+		State s0 = new StateTablut(), s1;
+		s0.setTurn(Turn.WHITE);
+
+		System.out.println("*" +s0.getTurn());
+
 		for (int i=0; i<9; i++)
 			for (int j=0; j<9; j++)
 				s0.getBoard()[i][j] = Pawn.EMPTY;
-		
-		s0.getBoard()[3][4] = Pawn.KING;
-		s0.getBoard()[2][4] = Pawn.BLACK;
-		s0.getBoard()[3][3] = Pawn.BLACK;
-		s0.getBoard()[8][5] = Pawn.BLACK;
-		s0.getBoard()[4][4] = Pawn.THRONE;
 
-		
+		//		s0.getBoard()[3][4] = Pawn.KING;
+		//		s0.getBoard()[2][4] = Pawn.WHITE;
+		//		s0.getBoard()[3][5] = Pawn.WHITE;
+		//		s0.getBoard()[8][5] = Pawn.WHITE; // LUI MANGIA in 2 mosse
+		//		s0.getBoard()[6][4] = Pawn.BLACK;
+
+		//s0.getBoard()[2][4] = Pawn.BLACK;
+		s0.getBoard()[5][3] = Pawn.BLACK;
+		s0.getBoard()[4][4] = Pawn.KING;
+		//s0.getBoard()[8][6] = Pawn.BLACK; // LUI MANGIA in 2 mosse
+
+
+
+		s1 = m.minimaxDecision(s0);
 		System.out.println(s0);
+		System.out.println(s1);
 
-		State s1 = m.minimaxDecision(s0);
-		
-		for (State s : m.successors(s0))
-			System.out.println(s + "\tutility: " + m.utility(s) + "\n\tis terminal: " + m.terminalTest(s));
 
-		System.out.println("\n\n\n------> " + s1);
 
 	}
 
