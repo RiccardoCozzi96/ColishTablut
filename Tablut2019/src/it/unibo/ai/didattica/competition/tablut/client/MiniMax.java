e9de798ad1b33bfa9d1c2810e3145b05866c01be
Administrator <Administrator@LAPTOP-8335SPHH>
2019-11-19 00:33:34 +0100

    Gioca ma ci mette troppo tempo (maxdepth 3).

--- Tablut2019/src/it/unibo/ai/didattica/competition/tablut/client/MiniMax.java
+++ Tablut2019/src/it/unibo/ai/didattica/competition/tablut/client/MiniMax.java
@@ -1,5 +1,6 @@
 package it.unibo.ai.didattica.competition.tablut.client;
 
+import java.time.LocalTime;
 import java.util.LinkedList;
 
 import it.unibo.ai.didattica.competition.tablut.domain.Action;
@@ -30,7 +31,7 @@ public class MiniMax {
 	public  int maxUtility = 0;
 	private Game game;
 	private Turn player;
-	private Integer maxDepth = 3;
+	private Integer maxDepth = 2;
 	private LinkedList<Action> eligibleActions;
 
 	/**
@@ -47,6 +48,8 @@ public class MiniMax {
 
 
 	public Action minimaxDecision(State state) {
+		LocalTime start = LocalTime.now();
+
 		this.eligibleActions.clear();
 		Integer depth = 0;
 		LinkedList<State> successors = successors(state, true);
@@ -63,7 +66,9 @@ public class MiniMax {
 			}
 			counter ++;
 		}
-		System.out.println("max: " + max);
+		//System.out.println("max: " + max);
+		LocalTime end = LocalTime.now();
+		System.out.println("\n\n\n___________\nmossa in circa : " + ((end.toSecondOfDay() - start.toSecondOfDay())) + " secondi");
 		return this.eligibleActions.get(bestMoveIndex);
 
 	}
@@ -95,7 +100,7 @@ public class MiniMax {
 		if (state.getTurn().equals(Turn.BLACKWIN) ||
 				state.getTurn().equals(Turn.WHITEWIN) || 
 				state.getTurn().equals(Turn.DRAW)) {
-			System.out.println("TURNO TERMINALE: " + state.getTurn());
+			//System.out.println("TURNO TERMINALE: " + state.getTurn());
 			return true;
 		} else return false;
 
@@ -165,18 +170,22 @@ public class MiniMax {
 
 						// up
 						for (int r = row-1; r >= 0; r--)	
-							if (board[r][col].equals(Pawn.EMPTY)) toTry.add(state.getBox(r, col));	else break;// free cells on vertical axis 
+							if (board[r][col].equals(Pawn.EMPTY) && !isCitadel(r, col)) toTry.add(state.getBox(r, col));	else break;// free cells on vertical axis 
 						// right
 						for (int c = col+1; c < dim; c++)	
-							if (board[row][c].equals(Pawn.EMPTY)) toTry.add(state.getBox(row, c));	else break;// free cells on horizontal axis 
+							if (board[row][c].equals(Pawn.EMPTY) && !isCitadel(row, c)) toTry.add(state.getBox(row, c));	else break;// free cells on horizontal axis 
 						// down
 						for (int r = row+1; r < dim; r++)	
-							if (board[r][col].equals(Pawn.EMPTY)) toTry.add(state.getBox(r, col));	else break; // free cells on vertical axis 
+							if (board[r][col].equals(Pawn.EMPTY) && !isCitadel(r, col)) toTry.add(state.getBox(r, col));	else break; // free cells on vertical axis 
 						// elft
 						for (int c = col-1; c >= 0; c--)	
-							if (board[row][c].equals(Pawn.EMPTY)) toTry.add(state.getBox(row, c));	else break;// free cells on horizontal axis 
+							if (board[row][c].equals(Pawn.EMPTY) && !isCitadel(row, c)) toTry.add(state.getBox(row, c));	else break;// free cells on horizontal axis 
+
+						// check citadel
+
 
 						for (String to:toTry) {
+
 							try {
 								Action a = new Action(from, to, state.getTurn());
 								elegibiles.add(game.checkMove(state.clone(), a));
@@ -190,6 +199,26 @@ public class MiniMax {
 	}
 
 
+	public boolean isCitadel(int row, int col) {
+		return (	((row == 0 || row == 8) && (col >= 3 && col <= 5)) ||
+				((col == 0 || col == 8) && (row >= 3 && row <= 5)) ||
+				((row == 1 || row == 7) && (col == 4)) ||
+				((col == 1 || col == 7) && (row == 4)) );
+
+		/*
+		 * TEST PER IL MAIN
+		 * 	for (int i=0; i<9; i++) {
+				for (int j=0; j<9; j++) {
+					if (m.isCitadel(i,  j))
+						System.out.print("X");
+					else
+						System.out.print(".");
+				}
+				System.out.print("\n");
+			}
+		}
+		 */
+	}
 
 
 
@@ -218,44 +247,50 @@ public class MiniMax {
 		s0.getBoard()[5][3] = Pawn.BLACK;
 		s0.getBoard()[4][4] = Pawn.KING;
 		//s0.getBoard()[8][6] = Pawn.BLACK; // LUI MANGIA in 2 mosse
-
-
+		
 		System.out.println("Stato 0" + s0);
 		try {
+			
+			LocalTime start = LocalTime.now();
+
 			a1 = m.minimaxDecision(s0); 
 			s1 = game.checkMove(s0, a1);
-			
-			
+
+
 			a2 = m2.minimaxDecision(s1); 
 			s2 = game.checkMove(s1, a2);
 
 			a3 = m.minimaxDecision(s2); 
 			s3 = game.checkMove(s2, a3);
 
-			
-			
+
 			System.out.println(s0 + "Azione 1: " + a1);
-			
+
 			System.out.println("Stato 1" + s1);
-			
+
 			System.out.println(s1 + "Azione 2: " + a2);
 			System.out.println("Stato 2" + s2);
-			
+
 			System.out.println(s2 + "Azione 3: " + a3);
 			System.out.println("Stato 3" + s3);
+			LocalTime end = LocalTime.now();
 
 			
-			
-			
-			
-			
-			
+			System.out.println("\n\n\n___________\nCirca : " + ((end.toSecondOfDay() - start.toSecondOfDay())) + " secondi");
+
+
+
+
+
 		} catch (BoardException | ActionException | StopException | PawnException | DiagonalException
 				| ClimbingException | ThroneException | OccupitedException | ClimbingCitadelException
 				| CitadelException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		 
+
+
 
 	}
 
